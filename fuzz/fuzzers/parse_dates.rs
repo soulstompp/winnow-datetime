72c723eea6b9ac0d2e1c1cc7da16fa29f5a0665b
Kenny Flegal <soulstompp@gmail.com>
2026-09-23 23:35:11 +0100

    Update CI and fuzz targets, drop release.toml

--- fuzz/fuzzers/parse_dates.rs
+++ fuzz/fuzzers/parse_dates.rs
@@ -1,12 +1,20 @@
 #![no_main]
 
-#[macro_use] extern crate libfuzzer_sys;
-extern crate iso8601;
+use libfuzzer_sys::fuzz_target;
 
-fuzz_target!(|data| {
+fuzz_target!(|data: &[u8]| {
     if let Ok(data) = std::str::from_utf8(data) {
-        let _ = iso8601::date(data);
-        let _ = iso8601::time(data);
-        let _ = iso8601::datetime(data);
+        let _ = winnow_iso8601::parse_date(data);
+        let _ = winnow_iso8601::parse_time(data);
+        let _ = winnow_iso8601::parse_datetime(data);
+        let _ = winnow_iso8601::parse_duration(data);
+
+        let _ = winnow_rfc3339::parse_date(data);
+        let _ = winnow_rfc3339::parse_time(data);
+        let _ = winnow_rfc3339::parse_datetime(data);
+
+        let _ = winnow_rfc9557::parse_date(data);
+        let _ = winnow_rfc9557::parse_time(data);
+        let _ = winnow_rfc9557::parse_datetime(data);
     }
 });
